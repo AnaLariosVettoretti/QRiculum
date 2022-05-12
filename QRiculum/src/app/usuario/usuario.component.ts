a03a06035f6075bb35c8eaf2295eea319d09d1e9
AnaLariosVettoretti <anavettoretti98@gmail.com>
2022-05-12 21:21:35 +0200

    mockeo terminado

--- QRiculum/src/app/usuario/usuario.component.ts
+++ QRiculum/src/app/usuario/usuario.component.ts
@@ -14,6 +14,18 @@ export class UsuarioComponent implements OnInit {
   @Input() email: string | undefined;
   constructor() { }
 
+  user = {
+    usuario: 'JuanPerez95',
+    nombre: 'Juan',
+    apellido1: 'Pérez',
+    apellido2: 'García',
+    email: 'jupergar@gmail.com',
+    contraseña: '123456',
+    telefono1: '678453275',
+    telefono2: '643652785',
+    ciudad: 'Madrid'  
+  }
+
   ngOnInit() {
   }
 
