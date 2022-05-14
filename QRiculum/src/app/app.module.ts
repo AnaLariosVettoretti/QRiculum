36e57ca096892b1bf7fd44e2d443de149788f4de
AnaLariosVettoretti <anavettoretti98@gmail.com>
2022-05-14 15:09:41 +0200

    binding bidireccional

--- QRiculum/src/app/app.module.ts
+++ QRiculum/src/app/app.module.ts
@@ -7,7 +7,7 @@ import { LoginComponent } from './login/login.component';
 import { RegistroComponent } from './registro/registro.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatFormFieldModule} from '@angular/material/form-field';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import {MatInputModule} from '@angular/material/input';
 import {MatSelectModule} from '@angular/material/select';
 import {MatIconModule} from '@angular/material/icon';
@@ -50,6 +50,7 @@ import { CvComponent } from './cv/cv.component';
     MatListModule,
     MatButtonModule,
     MatDatepickerModule,
+    FormsModule,
   ],
 
   providers: [],
