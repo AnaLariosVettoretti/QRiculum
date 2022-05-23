b03971d6e4b49a1aa59d09b3f8385817100ea1b9
AnaLariosVettoretti <anavettoretti98@gmail.com>
2022-05-23 18:12:32 +0200

    Mostrar cabecera sólo si hay sesión

--- QRiculum/src/app/app.component.ts
+++ QRiculum/src/app/app.component.ts
@@ -5,6 +5,19 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'QRiculum';
+  usuario: any = '';
+
+  ngOnInit() {
+    if (sessionStorage.getItem('usuario')) {
+      this.usuario = sessionStorage.getItem('usuario');
+    }
+
+  }
+
 }
+
+
+
