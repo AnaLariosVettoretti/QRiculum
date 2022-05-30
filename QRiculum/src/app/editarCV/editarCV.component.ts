19f18317c36d7d2e702b4656259e619534efa2ab
AnaLariosVettoretti <anavettoretti98@gmail.com>
2022-05-30 22:22:12 +0200

    Arreglando formulario dinámico

--- QRiculum/src/app/editarCV/editarCV.component.ts
+++ QRiculum/src/app/editarCV/editarCV.component.ts
@@ -11,7 +11,10 @@ import { FormGroup, FormControl, FormBuilder, FormArray } from "@angular/forms";
 export class EditarCVComponent implements OnInit {
 
   user: any;
-  datosCV: any;
+  formacionForm: any = FormGroup;
+  //datosCV: FormGroup;
+  //formData;
+
 
   constructor(private miServicio: ServicioUsuariosService, private route: ActivatedRoute, private fb: FormBuilder) { }
 
@@ -20,35 +23,182 @@ export class EditarCVComponent implements OnInit {
     //Coge nombre de usuario de la URL para hacer la petición al servicio y obtener el CV
     const username = this.route.snapshot.queryParamMap.get('username');
 
+
     if (username) {
       const user = this.miServicio.obtenerCVUsuario(username);
       this.user = user;
+      console.log('entras?', this.user)
     }
 
-    //Inicializa formulario
-    this.initializeForm();
 
+
+    this.formacionForm = this.fb.group({
+      formacion: this.fb.array([])
+    });
+
+    this.addFakeFormacion()
+
+    console.log('formacion', this.formacionForm.value)
+
+    this.formacionForm = this.fb.group({
+      sobreMi: this.fb.control,
+      redes: this.fb.array([]),
+      formacion: this.fb.array([]),
+      expLaboral: this.fb.array([]),
+    });
+
+
+    //   this.formData = getFormData(this.user.cv);
+
+    //   console.log(this.user.cv)
+
+    //   function getFormData(object) {
+    //     const formData = new FormData();
+    //     Object.keys(object).forEach(key => formData.append(key, object[key]));
+    //     return formData;
+    // }
+
+
+
+
+    //Inicializa formulario
+    //this.initializeForm(this.formData);
+
+  }
+
+  newFakeData(): FormGroup {
+    return this.fb.group({
+      institucion: 'Ies Test',
+      denominacion: 'Superior',
+      descripcion: 'Test',
+      nota: 2,
+      fIni: '22-09-2022',
+      fFin: '22-06-2023'
+
+    });
+  }
+
+  addFakeFormacion() {
+    this.formacion().push(this.newFakeData());
   }
 
   guardarCambios() {
     /* console.log(this.user); */
   }
 
-  initializeForm(): void {
+  // initializeForm(formData): void {
 
-    this.datosCV = this.fb.group({
-      sobreMi: this.user.cv.sobreMi,
-      formacion: this.fb.array([
-        this.fb.control('')
-      ])
-    })
+  //   /* this.datosCV = this.fb.group({
+  //     sobreMi: this.user.cv.sobreMi,
+  //     formacion: this.fb.array([
+  //       this.fb.control('')
+  //     ])
+  //   }) */
+
+  //   let form ={};
+
+  //   for (let i = 0; i < formData.length; i++) {
+  //     
+  //     form[formData[i].label] = new FormControl(formData.value);
+  //     console.log(form);
+  //     
+  //   }
+
+  //   this.datosCV = new FormGroup(form);
+  //   
+  // }
+
+  // onSubmit(): void {
+  //   console.log(this.datosCV);
+  // }
+
+  // getFormacion(): FormArray {
+  //   return this.datosCV?.get('formacion') as FormArray;
+  // }
+
+  // nuevaFormacion(): void {
+  //   this.formData.push({"sobreMi": ""});
+  // }
+
+  //REDES SOCIALES
+
+  redes(): FormArray {
+    return this.formacionForm.get('redes') as FormArray;
   }
 
-  onSubmit(): void {
-    console.log(this.datosCV);
+  newRedes(): FormGroup {
+    return this.fb.group({
+      red: '',
+      usuario: ''
+    });
   }
 
-  getFormacion(): FormArray{
-    return this.datosCV?.get('formacion') as FormArray;
+  addRedes() {
+    this.redes().push(this.newRedes());
   }
+
+  removeRedes(empIndex: number) {
+    this.redes().removeAt(empIndex);
+  }
+
+  //EXPERIENCIA LABORAL
+
+  expLaboral(): FormArray {
+    return this.formacionForm.get('expLaboral') as FormArray;
+  }
+
+  newExpLaboral(): FormGroup {
+    return this.fb.group({
+      empresa: '',
+      cargo: '',
+      descripcion: '',
+      fIni: null,
+      fFin: null
+    });
+  }
+
+  addExpLaboral() {
+    this.expLaboral().push(this.newExpLaboral());
+  }
+
+  removeExpLaboral(empIndex: number) {
+    this.expLaboral().removeAt(empIndex);
+  }
+
+  //FORMACIÓN
+
+  formacion(): FormArray {
+    return this.formacionForm.get('formacion') as FormArray;
+  }
+
+  newFormacion(): FormGroup {
+    return this.fb.group({
+      institucion: '',
+      denominacion: '',
+      descripcion: '',
+      nota: null,
+      fIni: null,
+      fFin: null
+
+
+      //skills: this.fb.array([])
+    });
+  }
+
+  addFormacion() {
+    this.formacion().push(this.newFormacion());
+  }
+
+  removeFormacion(empIndex: number) {
+    this.formacion().removeAt(empIndex);
+  }
+
+
+  onSubmit() {
+    console.log(this.formacionForm.value);
+  }
+
+
+
+
 }
